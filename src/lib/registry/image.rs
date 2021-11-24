6d06f08d9eac6cc363a36fd97529315ac76fb01a
shellcodesniper <shellcodesniper@icloud.com>
2021-11-24 11:46:30 +0900

    Fix Docker Registry Failed

--- src/lib/registry/image.rs
+++ src/lib/registry/image.rs
@@ -42,31 +42,33 @@ impl Registry {
     
     let res_body =  hyper::body::to_bytes(req.into_body()).await.unwrap();
     let res_string = String::from_utf8(res_body.to_vec()).unwrap();
-    let result: serde_json::Value = serde_json::from_str(&res_string).unwrap();
+    let result = serde_json::from_str(&res_string);
 
 
+    if result.is_ok() {
+      let result: serde_json::Value = result.unwrap();
+      if result.get("count").is_some() {
+        let result = result.get("results").unwrap().as_array().unwrap();
+        for result_idx in 0..result.len(){
+          let target_row = result.get(result_idx).unwrap();
+          let digest = target_row.get("digest").unwrap().as_str().unwrap();
+          let tags = target_row.get("tags");
+          if tags.is_some() {
+            let tags = target_row.get("tags").unwrap().as_array().unwrap();
+            for tag_idx in 0..tags.len() {
+              let tag_target= tags.get(tag_idx).unwrap();
+              let tag_str = tag_target.get("tag").unwrap().as_str().unwrap();
+              let tag_is_current = tag_target.get("is_current").unwrap().as_bool().unwrap();
+              // debug!("DIGEST: {}, TAG: {} {}", digest, tag_str, tag_is_current);
 
-    if result.get("count").is_some() {
-      let result = result.get("results").unwrap().as_array().unwrap();
-      for result_idx in 0..result.len(){
-        let target_row = result.get(result_idx).unwrap();
-        let digest = target_row.get("digest").unwrap().as_str().unwrap();
-        let tags = target_row.get("tags");
-        if tags.is_some() {
-          let tags = target_row.get("tags").unwrap().as_array().unwrap();
-          for tag_idx in 0..tags.len() {
-            let tag_target= tags.get(tag_idx).unwrap();
-            let tag_str = tag_target.get("tag").unwrap().as_str().unwrap();
-            let tag_is_current = tag_target.get("is_current").unwrap().as_bool().unwrap();
-            // debug!("DIGEST: {}, TAG: {} {}", digest, tag_str, tag_is_current);
-
-            if tag_is_current {
-              if tag_str == &search_target_tag {
-                return IGetDigestResult {
-                  found: true,
-                  digest: Some(digest.to_string()),
-                  image_url: image_url.clone(),
-                  tag: search_target_tag,
+              if tag_is_current {
+                if tag_str == &search_target_tag {
+                  return IGetDigestResult {
+                    found: true,
+                    digest: Some(digest.to_string()),
+                    image_url: image_url.clone(),
+                    tag: search_target_tag,
+                  }
                 }
               }
             }
@@ -74,6 +76,7 @@ impl Registry {
         }
       }
     }
+
 
     return IGetDigestResult {
       found: false,
