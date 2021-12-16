adb6fe3c4b1c01c250c07f7f38e064604af3beaf
Oracle Public Cloud User <opc@bdev.io>
2021-12-16 11:43:00 +0000

    Fix Config Location

--- bin/start_nossl.sh
+++ bin/start_nossl.sh
@@ -1,21 +1,22 @@
 docker swarm init
 docker network create --opt encrypted --driver overlay --attachable overlay
 
-rm -rf /tmp/kuuwange/
-mkdir -p /tmp/kuuwange/nginx/
-mkdir -p /tmp/kuuwange/certs/
+sudo rm -rf /etc/kuuwange/
+sudo mkdir -p /etc/kuuwange/nginx/
+sudo mkdir -p /etc/kuuwange/certs/
+sudo chown -R ${USER:=$(/usr/bin/id -run)}:$USER /etc/kuuwange/
 mkdir -p $(pwd)/logs/
 
-cp -rf ./nginx_template/nossl/* /tmp/kuuwange/nginx/
-cp ./config.cfg /tmp/kuuwange/config.cfg
-cp ./regenerate.sh /tmp/kuuwange/nginx/
+cp -rf ./nginx_template/nossl/* /etc/kuuwange/nginx/
+cp ./config.cfg /etc/kuuwange/config.cfg
+cp ./regenerate.sh /etc/kuuwange/nginx/
 
 docker pull shellcodesniper/simpledeploy:stable
 
 docker run -d --rm --name deployer \
   --network=overlay \
-  -v /tmp/kuuwange/:/tmp/kuuwange \
+  -v /etc/kuuwange/:/etc/kuuwange \
   -v $(pwd)/logs/:/app/logs/ \
   -v /var/run/docker.sock:/var/run/docker.sock \
     shellcodesniper/simpledeploy:stable \
-      /tmp/kuuwange/config.cfg
+      /etc/kuuwange/config.cfg
